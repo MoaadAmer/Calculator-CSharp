a5a59c448531d63e5c4f1cf3bd7895d09accc52a
Moaad Amer <moaad02@gmail.com>
2023-11-09 12:50:33 +0200

    first version support addition,subtraction and multiplication

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+Console.WriteLine("Hello!");
+Console.WriteLine("Input the first number:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Input the second number:");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[A]dd numbers");
+Console.WriteLine("[S]ubtract numbers");
+Console.WriteLine("[M]ultiply numbers");
+string operation = Console.ReadLine();
+operation = operation.ToUpper();
+
+switch (operation)
+{
+    case "A":
+        Console.WriteLine($"{a}+{b}={a + b}");
+        break;
+    case "S":
+        Console.WriteLine($"{a}-{b}={a - b}");
+        break;
+    case "M":
+        Console.WriteLine($"{a}*{b}={a * b}");
+        break;
+    default:
+        Console.WriteLine("invalid choice!");
+        Console.WriteLine("press anykey to close");
+        Console.ReadLine();
+        break;
+}
